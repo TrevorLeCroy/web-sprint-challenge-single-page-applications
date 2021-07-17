a2c18bc10004350aa3a8c3be7ea61546db5f5734
trevorlecroy <trevorlecroy@hotmail.com>
2021-07-17 18:22:51 -0400

    Set up needed files, and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import PizzaForm from "./PizzaForm";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+      <header>
+        <Link to='/'><h1>Lambda Eats</h1></Link>
+      </header>
+
+      <Route exact path='/'>
+        <Link to='/BYOP'> Pizza </Link>
+      </Route>
+
+      <Route path='/BYOP'> 
+        <PizzaForm></PizzaForm>
+      </Route>
+
+      <Route path='/submission'>
+        
+      </Route>
+    </Router>
   );
 };
 export default App;
---
+++ src/PizzaForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const PizzaForm = props => {
+    return (
+        <form id='pizza-form'>
+            <h2> Build Your Own Pizza </h2>
+            <div> </div>
+        </form>
+    );
+}
+
+export default PizzaForm;
