0cbc2e4e880ddb466d8fc5fef7fcc72054e42711
trevorlecroy <trevorlecroy@hotmail.com>
2021-07-17 18:56:56 -0400

    Added in various handlers, and completed Pizza Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import PizzaForm from "./PizzaForm";
+//import SubmissionForm from './SubmissionForm';
+import * as yup from 'yup';
+import axios from 'axios';
+
+let pizzaSchema = yup.object().shape({
+  name:            yup.string().min(2, 'name must be at least 2 characters').required('Name is Required'),
+  size:            yup.string().required('You must select a pizza size').oneOf(['Small', 'Medium', 'Large', 'Extra Large'], 'You must select a pizza size'),
+  pepperoni:       yup.boolean(),
+  sausage:         yup.boolean(),
+  can_bacon:       yup.boolean(),
+  si_sausage:      yup.boolean(),
+  grilled_chicken: yup.boolean(),
+  onions:          yup.boolean(),
+  green_pepper:    yup.boolean(),
+  diced_tomatos:   yup.boolean(),
+  black_olives:    yup.boolean(),
+  roasted_garlic:  yup.boolean(),
+  three_cheese:    yup.boolean(),
+  pineapple:       yup.boolean(),
+  special:         yup.string()
+});
 
 const App = () => {
+  const defaultPizza = {
+    name:            '',
+    size:            '',
+    pepperoni:       false,
+    sausage:         false,
+    can_bacon:       false,
+    si_sausage:      false,
+    grilled_chicken: false,
+    onions:          false,
+    green_pepper:    false,
+    diced_tomatos:   false,
+    black_olives:    false,
+    roasted_garlic:  false,
+    three_cheese:    false,
+    pineapple:       false,
+    special:         ''
+  };
+
+  const [pizza, setPizza]   = useState(defaultPizza);
+  const [errors, setErrors] = useState(defaultPizza);
+
+  const setFormErrors = (name, value) => {
+    yup.reach(pizzaSchema, name)
+      .validate(value)
+      .then(() => {
+        setErrors({...errors, [name] : ''});
+      })
+      .catch(err => {
+        setErrors({...errors, [name] : err.errors[0]})
+      });
+  }
+
+  const submitHandler = event => {
+    event.preventDefault();
+    
+    // new Pizza
+
+    // axios POST Requst
+
+    // Reset form here
+  }
+
+  const handleChange = event => {
+    const {name, type, value, checked} = event.target;
+    const updatedInfo                  = type === 'checkbox' ? checked : value;
+
+    setFormErrors(name, updatedInfo);
+    setPizza({
+      ...pizza,
+      [name]: updatedInfo
+    });
+  }
+
+  // For enabling / disabling our submit button
+  // useEffect(() => {
+  //   pizzaSchema.isValid(pizza).then(value => setDisabled(!valid))
+  // }, [pizza])
+
   return (
     <Router>
       <header>
@@ -10,15 +89,15 @@ const App = () => {
       </header>
 
       <Route exact path='/'>
-        <Link to='/BYOP'> Pizza </Link>
+        <Link to='/BYOP' id='order-pizza'> Order Pizza </Link>
       </Route>
 
       <Route path='/BYOP'> 
-        <PizzaForm></PizzaForm>
+        <PizzaForm form={pizza} submitHandler={submitHandler} handleChange={handleChange}> </PizzaForm>
       </Route>
 
       <Route path='/submission'>
-        
+
       </Route>
     </Router>
   );
--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -3,9 +3,118 @@ import React from 'react';
 
 const PizzaForm = props => {
     return (
-        <form id='pizza-form'>
+        <form id='pizza-form' onSubmit={props.submitHandler}>
             <h2> Build Your Own Pizza </h2>
-            <div> </div>
+            <div> 
+                <label> Pizza Name</label>
+                <input 
+                       id='name-input'
+                       name='name'
+                       type='text'
+                       value={props.form.name}
+                       onChange={props.handleChange}>
+                </input>
+
+                <label> Pizza Size </label>
+                <select 
+                        id='size-dropdown'
+                        name='size'
+                        type='dropdown'
+                        value={props.form.size}
+                        onChange={props.handleChange}>
+                    <option> Small </option>
+                    <option> Medium </option>
+                    <option> Large </option>
+                    <option> Extra Large </option>
+                </select>
+
+                <div className='pizza-toppings'>
+                    <label> Pepperoni </label>
+                    <input name='pepperoni'
+                        type='checkbox'
+                        value={props.form.pepperoni}
+                        onChange={props.handleChange}> 
+                    </input>
+
+                    <label> Sausage </label>
+                    <input name='sausage'
+                        type='checkbox'
+                        value={props.form.sausage}
+                        onChange={props.handleChange}> 
+                    </input>
+
+                    <label> Canadian Bacon </label>
+                    <input name='can_bacon'
+                        type='checkbox'
+                        value={props.form.can_bacon}
+                        onChange={props.handleChange}> 
+                    </input>
+
+                    <label> Spicy Italian Sausage </label>
+                    <input name='si_sausage'
+                        type='checkbox'
+                        value={props.form.si_sausage}
+                        onChange={props.handleChange}> 
+                    </input>
+
+                    <label> Grilled Chicken </label>
+                    <input name='grilled_chicken'
+                        type='checkbox'
+                        value={props.form.grilled_chicken}
+                        onChange={props.handleChange}> 
+                    </input>
+
+                    <label> Onions </label>
+                    <input name='onions'
+                        type='checkbox'
+                        value={props.form.onions}
+                        onChange={props.handleChange}> 
+                    </input>
+
+                    <label> Green Pepper </label>
+                    <input name='green_pepper'
+                        type='checkbox'
+                        value={props.form.green_pepper}
+                        onChange={props.handleChange}> 
+                    </input>
+
+                    <label> Diced Tomatos </label>
+                    <input name='diced_tomatos'
+                        type='checkbox'
+                        value={props.form.diced_tomatos}
+                        onChange={props.handleChange}> 
+                    </input>
+
+                    <label> Black Olives </label>
+                    <input name='black_olives'
+                        type='checkbox'
+                        value={props.form.black_olives}
+                        onChange={props.handleChange}> 
+                    </input>
+
+                    <label> Roasted Garlic </label>
+                    <input name='roasted_garlic'
+                        type='checkbox'
+                        value={props.form.roasted_garlic}
+                        onChange={props.handleChange}> 
+                    </input>
+
+                    <label> Three Cheese </label>
+                    <input name='three_cheese'
+                        type='checkbox'
+                        value={props.form.three_cheese}
+                        onChange={props.handleChange}> 
+                    </input>
+                </div>
+
+                <label> Special Instructions </label> 
+                <input id='special-text'
+                       name='special'
+                       type='text'
+                       value={props.form.special}
+                       onChange={props.handleChange}>
+                </input>
+            </div>
         </form>
     );
 }
